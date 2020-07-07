3373bac15ceafd5a71381888c921283a798165e0
Syahiid Nur Kamil <hiid.aicode@gmail.com>
2020-07-07 23:41:34 +0700

    hari ketiga

---
+++ 1-60/dasar.js
@@ -0,0 +1,32 @@
+console.log('hello, world');
+
+alert('peringatan');
+
+const constanta = 314;
+console.log(constanta);
+alert(constanta);
+
+const  a = 101;
+alert(a);
+
+const b = 102;
+alert(b);
+
+const c = 103;
+alert(c);
+
+const d = 104;
+console.log(d);
+
+const e = 105;
+console.log(e);
+
+const f = 106;
+console.log(f);
+
+const a = 107;
+console.log(a);
+
+const a = 108;
+console.log(a);
+
