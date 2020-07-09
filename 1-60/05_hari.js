3c55761430bf89a86c5be4d9ff77fb64005798f6
Syahiid Nur Kamil <hiid.aicode@gmail.com>
2020-07-09 23:19:02 +0700

    hari ke 5

---
+++ 1-60/05_hari.js
@@ -0,0 +1,19 @@
+// Data types
+
+// string
+const string = '';
+
+// number
+const number = 123;
+
+// array
+const array = ['barang1', 'barang2', 123];
+
+// object
+const object = {
+  nama: 'hello',
+  sekolah: 'sma koding'
+};
+
+// boolean
+const boolean = true;
